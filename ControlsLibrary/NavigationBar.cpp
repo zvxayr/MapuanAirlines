0889bf65124af6cadd645cbc67f99e11a39ace6e
zvxayr <carlo4noles@gmail.com>
2020-10-02 18:59:07 +0800

    Added NavigationBar

---
+++ ControlsLibrary/NavigationBar.cpp
@@ -0,0 +1,56 @@
+#include "pch.h"
+#include "NavigationBar.h"
+
+using namespace ControlsLibrary;
+
+NavigationBar::NavigationBar()
+{
+	InitializeComponent();
+}
+
+NavigationBar::~NavigationBar()
+{
+	if (components)
+		delete components;
+}
+
+void NavigationBar::SetActiveButton(System::Windows::Forms::Button^ button) {
+	if (m_ActiveButton)
+		m_ActiveButton->BackColor = System::Drawing::Color::White;
+
+	button->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(253)),
+		static_cast<System::Int32>(static_cast<System::Byte>(215)), static_cast<System::Int32>(static_cast<System::Byte>(75)));
+
+	m_ActiveButton = button;
+}
+
+void NavigationBar::ClearActiveButton() {
+	if (m_ActiveButton)
+		m_ActiveButton->BackColor = System::Drawing::Color::White;
+
+	m_ActiveButton = nullptr;
+}
+
+System::Void NavigationBar::m_BuyTicketsButton_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (m_BuyTicketsButton == m_ActiveButton) return;
+	SetActiveButton(m_BuyTicketsButton);
+	if (OnBuyTickets) OnBuyTickets();
+}
+
+System::Void NavigationBar::m_PaymentHistoryButton_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (m_PaymentHistoryButton == m_ActiveButton) return;
+	SetActiveButton(m_PaymentHistoryButton);
+	if (OnPaymentHistory) OnPaymentHistory();
+}
+
+System::Void NavigationBar::m_FlightStatusButton_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (m_FlightStatusButton == m_ActiveButton) return;
+	SetActiveButton(m_FlightStatusButton);
+	if (OnFlightStatus) OnFlightStatus();
+}
+
+System::Void NavigationBar::m_ExitButton_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (m_ExitButton == m_ActiveButton) return;
+	SetActiveButton(m_ExitButton);
+	if (OnExit) OnExit();
+}
